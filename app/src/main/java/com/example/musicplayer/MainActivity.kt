b70e7c65501a887c326e7ea4b206a7e72bad3b01
dtaolin01 <dtaolin01@student.ciputra.ac.id>
2026-09-25 14:28:40 +0700

    commit

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.musicplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
